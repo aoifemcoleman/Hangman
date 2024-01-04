b70d1aebff95a82437ed6f1e1c4ef360c03d088f
Aoife Coleman <aoifemcoleman@hotmail.com>
2024-01-04 13:11:03 +0000

    Add function to check length of current word

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,14 @@
-/**
- * Generating random word from word-list.js using wordList as parameter.
- */
+// Generating random word from word-list.js using wordList as parameter.
 function chooseWord(wordList) {
     let randomWord = wordList[Math.floor(Math.random() * wordList.length)];
     return randomWord;
 }
 let currentWord = chooseWord(wordList);
 console.log(currentWord);
+
+// Check length of currentWord
+function wordLength(word) {
+    return word.length;
+}
+let currentWordLength = wordLength(currentWord);
+console.log(`${currentWordLength}`);
