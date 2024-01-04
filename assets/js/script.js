153e40f97db1c0be0065ee438bdf209470203351
Aoife Coleman <aoifemcoleman@hotmail.com>
2024-01-04 15:15:23 +0000

    Create function to create list of blank spaces based on amount of letters in random word

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ function chooseWord(wordList) {
     let randomWord = wordList[Math.floor(Math.random() * wordList.length)];
     return randomWord;
 }
+// Print currentWord to console
 let currentWord = chooseWord(wordList);
 console.log(currentWord);
 
@@ -10,5 +11,21 @@ console.log(currentWord);
 function wordLength(word) {
     return word.length;
 }
+// Print length of currentWord to console
 let currentWordLength = wordLength(currentWord);
 console.log(`${currentWordLength}`);
+
+// Create list of blank spaces using returned word length
+function createLetterSpaces(length) {
+    let letterSpaces = document.getElementById('letter-spaces');
+    // Clear content from within list
+    letterSpaces.innerHTML = '';
+    // Create loop to append list items to the ul with 'letter-spaces'
+    for (let i = 0; i < length; i++) {
+        let blankSpace = document.createElement('li');
+        blankSpace.textContent = '_';
+        blankSpace.className = 'letter';
+        letterSpaces.appendChild(blankSpace);
+    }
+}
+createLetterSpaces(currentWordLength);
